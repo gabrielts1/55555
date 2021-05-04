cd1b438332891b9869e70a54d07c5446d2d3a1e1
gabrielts1 <69881123+gabrielts1@users.noreply.github.com>
2021-05-04 18:42:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,92 @@
+var app = {
 
-let app = {
-	
-	inicializar: function(){
-		console.log("inicializar: function(){...");
-		document.addEventListener('deviceready', meuapp.onDeviceReady, false);
-		document.addEventListener('onResume', meuapp.retornoAoPrimeiroPlano, false);
-		document.addEventListener('resume', meuapp.retornoAoPrimeiroPlano, false);
-		},
-		
-	onMyDeviceReady: function(){
-	
-	console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-	document.getElementById("btnSelecionaContato").addEventListener('click', meuapp.selecionarContato);
-	},
-	
-	retornoAoPrimeiroPlano: function(){
-		console.log("retornoAoPrimeiroPlano:function(){...");
-		},
-		
-	selecionarContato : function(){
-		console.log("selecionarContato : function(){...");
-		navigator.contacts.pickContact(function(c){
-			console.log(c);
-		},function(err){
-			console.log("Contato nao selecionado");
-			console.log(err);
-		});
-		
+    selectedContact : null,
+    selectedContactErr : null,
+    
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('resume', this.onResume.bind(this), false);        
+    },
 
-			
-		
-		
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        //alert(navigator.contacts);
+        document.getElementById("bntSelContato").addEventListener("click", this.selecionaContato); 
+        document.getElementById("bntShowContact").addEventListener("click", this.showSelectedContact);
+    },
 
-}
+    onResume :function(resumeEvent) {
+        alert("onResume :function(resumeEvent) {...");
+        if(resumeEvent.pendingResult) {
+            if(resumeEvent.pendingResult.pluginStatus === "OK") {
+                var contact = navigator.contacts.create(resumeEvent.pendingResult.result);
+                this.successCallback(contact);
+            } else {                
+                this.failCallback(resumeEvent.pendingResult.result);
+            }
+        }
+    },
+    successCallback : function(contact){
+        alert("successCallback : function(contact){..."+ contact);
 
+        alert("alert(contact);");
+        alert(contact);
+                
+        app.selectedContact = contact;
 
- 
-document.addEventListener('deviceready', onDeviceReady, false);
+        alert("alert(JSON.stringify(contact));");
+        alert(JSON.stringify(contact));
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+        alert("alert(app.selectedContact);");
+        alert(app.selectedContact);
+    },
+    failCallback : function(err){
+        alert(err);
+    },
+    
+    showSelectedContact: function(){
+        alert(" ## showSelectedContact: function(){...");
+        document.getElementById("contactName").innerHTML = app.getSelectedContactName();
+        document.getElementById("contactImg").src = app.getSelectedContactImgSrc();
+    },
+    
+    selecionaContato: function(){
+        navigator.contacts.pickContact(function(contact){
+            alert("selecionaContato: function(){...  navigator.contacts.pickContact(function(contact){...");
+            alert('The following contact has been selected:' + JSON.stringify(contact));
+            alert("Contact:"+contact);
+            app.selectedContact = contact;
+        },function(err){
+            alert("selecionaContato: function(){...  function(err){...");
+            alert('Error: ' + err);
+            alert("err:"+err);
+            app.selectedContactErr = err;
+        });
+    },
+    getSelectedContactImgSrc : function(){
+        let c = app.selectedContact;
+        if(c.photos && c.photos.length) {
+	        return c.photos[0].value;
+	}
+        return "";
+    },
+    getSelectedContactName : function(){
+ 	let cN = app.selectedContact.displayName;
+	if(!cN || cN === "") {
+	    if(cN.name.formatted){
+                return cN.name.formatted;
+            }
+	    if(cN.name.givenName && cN.name.familyName) {
+                return cN.name.givenName +" "+c.name.familyName;
+            }
+	    return "Nome Nao Informado";
+	}
+	return cN;
+    }
+};
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+app.initialize();
